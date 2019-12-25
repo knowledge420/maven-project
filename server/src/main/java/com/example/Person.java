444218fc6de7407a1d9933ce638150ba6a96f955
CHENG-CHOU LAI <knowledge840420@gmail.com>
2019-12-25 23:54:28 +0800

    fixed FinalParametersCheck

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -13,7 +13,7 @@ public class Person {
   }
 
   //TODO: Add javadoc comment
-  public String person(String someone) {
+  public String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
